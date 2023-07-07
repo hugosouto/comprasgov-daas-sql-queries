f59ec921a531d4124b4784c5308225c5f44dfbe9
Hugo Souto <hugomsouto@gmail.com>
2023-07-07 12:08:50 -0300

    Update table structure

--- sql/[Datalake Homolog] Portais Integrados - Create tables.sql
+++ sql/[Datalake Homolog] Portais Integrados - Create tables.sql
@@ -1,17 +1,18 @@
---DROP TABLE seges_delog_cgscg_stage.pncp.portais_integrados;
+-- Drop table no Datalake Homologação (10.209.42.30)
+DROP TABLE seges_delog_cgscg_stage.pncp.portais_integrados;
 
+-- Creat table no Datalake Homologação (10.209.42.30)
 CREATE TABLE seges_delog_cgscg_stage.pncp.portais_integrados (
-	qt_contratos_pncp int,
-	qt_compras int,
-	datainclusao varchar(10),
 	id int,
-	nomerazaosocial varchar(max),
-	cpfcnpj varchar(max),
+	nomerazaosocial varchar(200),
+	cpfcnpj varchar(14),
+	datainclusao datetime
 )
 
-INSERT INTO seges_delog_cgscg_stage.pncp.portais_integrados (qt_contratos_pncp,qt_compras,datainclusao,id,nomerazaosocial,cpfcnpj)
-VALUES (8355,79251,'05/08/2021', 3,'Compras.gov.br','00394460000141');
-INSERT INTO seges_delog_cgscg_stage.pncp.portais_integrados (qt_contratos_pncp,qt_compras,datainclusao,id,nomerazaosocial,cpfcnpj)
-VALUES (7986,76728,'05/08/2021',3,'Compras.gov.br','00394460000141');
-INSERT INTO seges_delog_cgscg_stage.pncp.portais_integrados (qt_contratos_pncp,qt_compras,datainclusao,id,nomerazaosocial,cpfcnpj)
-VALUES (665,3100,'09/08/2021',5,'ECustomize Consultoria em Software S.A','09397355000130');
+-- Insere dados artificiais para teste
+INSERT INTO seges_delog_cgscg_stage.pncp.portais_integrados (id,nomerazaosocial,cpfcnpj,datainclusao)
+VALUES (3,'Compras.gov.br','00394460000141', '05/08/2021 00:00:00.000');
+INSERT INTO seges_delog_cgscg_stage.pncp.portais_integrados (id,nomerazaosocial,cpfcnpj,datainclusao)
+VALUES (3,'Compras.gov.br','00394460000141', '05/08/2021 00:00:00.000');
+INSERT INTO seges_delog_cgscg_stage.pncp.portais_integrados (id,nomerazaosocial,cpfcnpj,datainclusao)
+VALUES (5,'ECustomize Consultoria em Software S.A','09397355000130', '09/08/2021 00:00:00.000');
