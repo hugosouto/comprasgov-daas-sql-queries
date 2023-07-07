9c1587573d5362c1184b5203a10ffa719f0abf9a
Hugo Souto <hugomsouto@gmail.com>
2023-07-07 12:08:35 -0300

    Update to working query

--- sql/[PNCP] Portais Integrados.sql
+++ sql/[PNCP] Portais Integrados.sql
@@ -1,3 +1,49 @@
+##### Script Atualizado
+
+SELECT
+	u.id,
+	u.nomerazaosocial,
+	u.cpfcnpj,
+	u.datainclusao
+FROM PNCP_VBL.usuario u
+JOIN (
+	SELECT co.usuarioid
+	FROM PNCP_VBL.contrato co
+	WHERE co.excluido = 0
+	UNION
+	SELECT c.usuarioid
+	FROM PNCP_VBL.compra c
+	WHERE c.excluido = 0
+	) AS active_users 
+    ON u.id = active_users.usuarioid
+ORDER BY u.id;
+
+
+##### Script Alterado
+
+SELECT
+	COUNT(DISTINCT ct.id) AS qt_contratos_pncp,
+	COUNT(DISTINCT cp.id) AS qt_compras,
+	SUBSTRING(u.datainclusao, 9, 2)|| '/' || SUBSTRING(u.datainclusao, 6, 2)|| '/' || SUBSTRING(u.datainclusao, 1, 4) datainclusao,
+	u.id,
+	u.nomerazaosocial,
+	u.cpfcnpj
+FROM usuario u
+	JOIN compra cp ON u.id = cp.usuarioid
+	JOIN contrato ct ON u.id = ct.usuarioid
+WHERE 1=1
+    AND (ct.id IS NOT NULL OR cp.id IS NOT NULL) 
+    AND ct.excluido = 0
+    AND cp.excluido = 0   
+GROUP BY
+	u.id,
+	u.nomerazaosocial,
+	u.cpfcnpj,
+	u.datainclusao
+ORDER BY
+	u.id;
+	
+
 ##### Script Original
 
 SELECT 
@@ -28,26 +74,3 @@ ORDER BY
     u.id;
 
 
-##### Script Alterado
-
-SELECT
-	COUNT(DISTINCT ct.id) AS qt_contratos_pncp,
-	COUNT(DISTINCT cp.id) AS qt_compras,
-	SUBSTRING(u.datainclusao, 9, 2)|| '/' || SUBSTRING(u.datainclusao, 6, 2)|| '/' || SUBSTRING(u.datainclusao, 1, 4) datainclusao,
-	u.id,
-	u.nomerazaosocial,
-	u.cpfcnpj
-FROM usuario u
-	JOIN compra cp ON u.id = cp.usuarioid
-	JOIN contrato ct ON u.id = ct.usuarioid
-WHERE 1=1
-    AND (ct.id IS NOT NULL OR cp.id IS NOT NULL) 
-    AND ct.excluido = 0
-    AND cp.excluido = 0   
-GROUP BY
-	u.id,
-	u.nomerazaosocial,
-	u.cpfcnpj,
-	u.datainclusao
-ORDER BY
-	u.id;
