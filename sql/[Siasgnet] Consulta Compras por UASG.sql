787786e74175d4cde762bade04701e071e0db873
Hugo Souto <hugomsouto@gmail.com>
2023-07-18 17:46:21 -0300

    Improve query

--- sql/[Siasgnet] Consulta Compras por UASG.sql
+++ sql/[Siasgnet] Consulta Compras por UASG.sql
@@ -1,6 +1,9 @@
 SELECT 
-	numerouasgorigem,
-	COUNT(codigocompra) contagemcompras
-FROM Siasgnet_dc_VBL.compra
+	o.numeroorgao,
+	c.numerouasgorigem,
+	COUNT(c.codigocompra) contagemcompras
+FROM Siasgnet_dc_VBL.compra c
+JOIN Siasgnet_VBL.uasg u ON c.numerouasgresponsavel = u.numerouasg
+JOIN Siasgnet_VBL.orgao o ON u.numeroorgaouasg = o.numeroorgao
 WHERE anocompra='2023' AND numerouasgorigem IN (390004, 399008, 400040, 400041, 400042, 400043, 400046, 400047, 400048, 400054, 400055, 400056, 400057, 400059, 400060, 400061, 400062, 400063, 400064, 400065, 400066, 400067, 400069, 400070, 400071, 400072, 400073, 400074, 400075, 400076, 400077, 400078, 400079, 400080, 400081, 400082, 400083, 400084, 400085, 400086, 400087, 400045, 400068)
-GROUP BY numerouasgorigem
+GROUP BY c.numerouasgorigem, c.numerouasgorigem, o.numeroorgao
