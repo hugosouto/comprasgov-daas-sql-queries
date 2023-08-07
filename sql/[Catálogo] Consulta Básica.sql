cdca6b4a38fa37c5723b102249fbbbe8c81b9ac1
Hugo Souto <hugomsouto@gmail.com>
2023-08-07 09:41:42 -0300

    Add 'grupos' and 'classes' queries

--- sql/[Catálogo] Consulta Básica.sql
+++ sql/[Catálogo] Consulta Básica.sql
@@ -1,7 +1,27 @@
 -- Estrutura:
--- - Classificação até PDM: vai mostrar apenas a estrutura hierárquica (20.063 linhas)
+-- - Classificação até Grupo: vai mostrar apenas a estrutura hierárquica até Grupo (79 linhas)
+-- - Classificação até Classe: vai mostrar apenas a estrutura hierárquica até Classe (718 linhas)
+-- - Classificação até PDM: vai mostrar apenas a estrutura hierárquica até PDM (20.063 linhas)
 -- - Classificação até Itens: vai exibir todos os códigos de itens válidos, mas sem o detalhamento (310.018 linhas)
 -- - Classificação até Características: se refere à tabela final, mas, devido à nova perspectiva de "Características", o detalhamento dos itens � mostrado de forma parametrizada, sendo cada linha uma caracter�stica de cada itens, gerando uma tabela bem maior do que a quantidade real de itens cadastrados (1.536.089 linhas)
+
+----- Classificação até Grupos
+SELECT 
+	g.codigo_grupo,
+	g.nome_grupo
+FROM grupo_material g
+;
+
+----- Classificação até Classe
+SELECT 
+	g.codigo_grupo,
+	g.nome_grupo,
+	c.codigo_classe,
+	c.nome_classe
+FROM grupo_material g
+JOIN classe_material c ON g.codigo_grupo = c.codigo_grupo
+ORDER BY g.codigo_grupo
+;
 
 ----- Classificação até PDM
 SELECT 
