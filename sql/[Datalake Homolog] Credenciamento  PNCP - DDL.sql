7f3d8426fca3c35a0a67532e1c1fb99a11ea1a4a
Hugo Souto <hugomsouto@gmail.com>
2024-04-18 00:32:37 -0300

    Add SQL script for Credenciamento PNCP in Datalake Homolog

---
+++ sql/[Datalake Homolog] Credenciamento  PNCP - DDL.sql
@@ -0,0 +1,24 @@
+CREATE SCHEMA credenciamento_pncp;
+
+CREATE TABLE atividade_principal (
+	cnpj_atividade_principal VARCHAR(14),
+	codigo_atividade_principal VARCHAR(10),
+	descricao_atividade_principal VARCHAR(MAX)
+	);
+
+INSERT atividade_principal VALUES ('','','');
+
+ALTER SCHEMA credenciamento_pncp
+TRANSFER dbo.atividade_principal;
+
+--DROP TABLE dbo.natureza_juridica;
+
+CREATE TABLE credenciamento_pncp.natureza_juridica (
+	cnpj_natureza_juridica VARCHAR(14),
+	cod_natureza_juridica VARCHAR(5),
+	descricao_natureza_juridica VARCHAR(MAX)
+	);
+
+INSERT credenciamento_pncp.natureza_juridica VALUES ('','','');
+	
+		
