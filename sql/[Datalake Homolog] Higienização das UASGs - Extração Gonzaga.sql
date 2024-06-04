8ca840b2c78f0c10791373e58083e1aa686fd8ee
Hugo Souto <hugomsouto@gmail.com>
2024-06-04 01:56:45 -0300

    Update schema

--- sql/[Datalake Homolog] Higienização das UASGs - Extração Gonzaga.sql
+++ sql/[Datalake Homolog] Higienização das UASGs - Extração Gonzaga.sql
@@ -1,13 +1,13 @@
 ----- siasg_orgaos
 SELECT * 
-FROM seges_delog_cgscg_stage.dbo.siasg_orgaos
+FROM seges_delog_cgscg_stage.hierarquia_uasgs.siasg_orgaos
 --WHERE 
 --	IT_IN_STATUS != '*'				-- Somente UASG�s com (in_status = diferente de �*� ? UASG�s Ativa;			
 ;
 
 ----- siasg_uasgs
 SELECT * 
-FROM seges_delog_cgscg_stage.dbo.siasg_uasgs
+FROM seges_delog_cgscg_stage.hierarquia_uasgs.siasg_uasgs
 --WHERE 
 --	IT_IN_STATUS != '*'				-- Somente UASG�s com (in_status = diferente de �*� ? UASG�s Ativa;
 --	AND IT_IN_ADESAO_SIASG = 'S'	-- Somente  UASG�s com it_in_adesao_siasg = �S� ?Aderiu ao SIASG
@@ -16,5 +16,5 @@ FROM seges_delog_cgscg_stage.dbo.siasg_uasgs
 
 ----- siasg_uasgs_orgaos
 SELECT * 
-FROM seges_delog_cgscg_stage.dbo.siasg_uasgs_orgaos
+FROM seges_delog_cgscg_stage.hierarquia_uasgs.siasg_uasgs_orgaos
 ;
