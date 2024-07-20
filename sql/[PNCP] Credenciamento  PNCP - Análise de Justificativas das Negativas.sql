f4b377c455b3bef09529e2be37feccb33f07e137
Hugo Souto <hugomsouto@gmail.com>
2024-07-19 21:43:33 -0300

    Update query

--- sql/[PNCP] Credenciamento  PNCP - Análise de Justificativas das Negativas.sql
+++ sql/[PNCP] Credenciamento  PNCP - Análise de Justificativas das Negativas.sql
@@ -1,6 +1,7 @@
-SELECT DISTINCT justificativaanalise/*SUBSTRING(justificativaanalise, 1, 100)*/, SUBSTRING(dataanaliseprocesso,1,4), COUNT(*)
+SELECT DISTINCT justificativaanalise/*SUBSTRING(justificativaanalise, 1, 100)*/, SUBSTRING(dataanaliseprocesso,1,4) dataanaliseprocesso, COUNT(*) contagem, COUNT(*)/CAST((SELECT COUNT(*) FROM credenciamento WHERE statuscredenciamentoid=2 AND SUBSTRING(dataanaliseprocesso,1,4) = 2024) AS FLOAT) percentual 
 FROM credenciamento
 WHERE statuscredenciamentoid=2 AND SUBSTRING(dataanaliseprocesso,1,4) = 2024
 GROUP BY justificativaanalise /*SUBSTRING(justificativaanalise, 1, 100)*/, SUBSTRING(dataanaliseprocesso,1,4)
-HAVING COUNT(*) > 50
+--HAVING COUNT(*) > 50
+ORDER BY 3 DESC
 ;
